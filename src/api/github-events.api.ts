13e81da832bafdbf969f3bda4c94aa7a7e7c80e8
Navdeep Singh <navdeep.er@gmail.com>
2020-11-15 17:27:34 +0530

    Done

--- src/api/github-events.api.ts
+++ src/api/github-events.api.ts
@@ -2,7 +2,6 @@ import {GithubEvent, GithubIssue} from "./github-events.model";
 import axios from "axios";
 import {useQuery} from "react-query";
 
-
 export function useGithubIssueComments( user: string, repo: string) {
    
     const url = `/networks/${user}/${repo}/events?per_page=100`;
@@ -49,30 +48,5 @@ export function mapResult(data: GithubEvent[]): GithubIssue[] {
 
     results = data.map(modifyData);
 
-    console.log(results);
     return (results);
-    
-    // TODO:
-    //   1. Remove the hardcoded data below
-    //   2. Transform the GithubEvent[] data passed in as a parameter, to the expected GithubIssue[] return type
-    //      Transformation guidelines:
-    //      a. Filter the input data to contain only events of type `IssueCommentEvent`
-    //      b. Transform the data from each GithubEvent to a GithubIssue as follows:
-    //         GithubIssue.id = GithubEvent.payload.issue.id
-    //         GithubIssue.created_at = GithubEvent.payload.issue.created_at
-    //         GithubIssue.title = GithubEvent.payload.issue.title
-    //         GithubIssue.body = GithubEvent.payload.issue.body
-    //         GithubIssue.user.id = GithubEvent.payload.issue.user.id
-    //         GithubIssue.user.login = GithubEvent.payload.issue.user.login
-    //      d. Aggregate all the comments in the GithubEvent.payload.comment fields, into the GithubIssue.comments arrays, so
-    //         that each GithubIssue contains an array of related comments.
-    //         The mapping is as follows:
-    //         GithubIssue.comments[n].id = GithubEvent.payload.comment.id
-    //         GithubIssue.comments[n].created_at = GithubEvent.payload.comment.created_at
-    //         GithubIssue.comments[n].body = GithubEvent.payload.comment.body
-    //         GithubIssue.comments[n].user.id = GithubEvent.payload.comment.user.id
-    //         GithubIssue.comments[n].user.login = GithubEvent.payload.comment.user.login
-    //   5. Verify that all tests pass, by running `npm test`
-    //   6. Once the tests pass, verify that the app runs in a browser by running `npm start`
-    //      Note that the data will look different in the browser, as the browser will render live data from Github.
 }
